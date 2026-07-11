581a8febb92753f9b9a478fe4aa0b36fd0b608da
big-gorae <big.gorae.king@gmail.com>
2026-07-11 16:54:45 +0900

    fix: face bathroom 3D model forward

--- scripts/overlays/scene_3d_overlay_catalog.gd
+++ scripts/overlays/scene_3d_overlay_catalog.gd
@@ -10,7 +10,7 @@ static func build_definitions() -> Array[Dictionary]:
 			"model_path": "res://resource/3d_models/dripping_gaze/dripping_gaze.glb",
 			"camera_size": 4.0,
 			"model_position": Vector3(2.05, -0.58, 0.0),
-			"model_rotation_degrees": Vector3(-7.0, -24.0, 0.0),
+			"model_rotation_degrees": Vector3(0.0, 0.0, 0.0),
 			"target_height": 1.05,
 			"scale_multiplier": 1.0,
 			"light_energy": 1.15,
